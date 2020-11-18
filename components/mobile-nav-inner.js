984b22ee0ed2ff2251d0f2429f733c76dec9b3a1
Manjunath G <admin@manjusha.dev>
2020-11-18 11:02:48 +0530

    5th

--- components/mobile-nav-inner.js
+++ components/mobile-nav-inner.js
@@ -22,11 +22,10 @@ const MobileNavInner = () => {
         <div className="side-menu__sep"></div>
         <div className="side-menu__content">
           <p>
-            Lorem Ipsum is simply dummy text the printing and setting industry.
-            Lorm Ipsum has been the industry's stanard dummy text ever.{" "}
+            .{" "}
           </p>
           <p>
-            <a href="mailto:needhelp@apton.com">needhelp@apton.com</a> <br />{" "}
+            <a href="mailto:needhelp@apton.com">support@colorbazzi.freshdesk.com</a> <br />{" "}
             <a href="tel:888-999-0000">888 999 0000</a>
           </p>
           <div className="side-menu__social">
